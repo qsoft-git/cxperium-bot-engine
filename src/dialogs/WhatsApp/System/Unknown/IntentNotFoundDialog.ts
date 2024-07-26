23b2dceacd18f48318feac9e8f55da660e37bcd1
erdinccurebal <erdinccurebal@hotmail.com>
2024-07-26 09:57:12 +0300

    general update

--- src/dialogs/WhatsApp/System/Unknown/IntentNotFoundDialog.ts
+++ src/dialogs/WhatsApp/System/Unknown/IntentNotFoundDialog.ts
@@ -5,13 +5,39 @@ import {
 	TBaseDialogCtor,
 } from '../../../../index';
 
+// Environments.
+const {
+	FIRST_CALLBACK_REDIRECT_MAIN_MENU_STATUS,
+	FIRST_CALLBACK_REDIRECT_MAIN_MENU_ID,
+	FIRST_CALLBACK_REDIRECT_MAIN_MENU_NAME,
+} = process.env;
+
 export default class extends ServiceWhatsappBaseDialog implements IDialog {
 	constructor(data: TBaseDialogCtor) {
 		super(data);
 	}
 	async runDialog(): Promise<void> {
-		await this.sendMessage(
-			await this.getLocalizationText('understand_message'),
-		);
+		if (
+			FIRST_CALLBACK_REDIRECT_MAIN_MENU_STATUS &&
+			FIRST_CALLBACK_REDIRECT_MAIN_MENU_STATUS === 'true'
+		) {
+			await this.sendButtonMessage(
+				await this.getLocalizationText('understand_message'),
+				'',
+				'',
+				[
+					{
+						id: FIRST_CALLBACK_REDIRECT_MAIN_MENU_ID || 'main_menu',
+						title:
+							FIRST_CALLBACK_REDIRECT_MAIN_MENU_NAME ||
+							'🔙 Ana Menüye Dön',
+					},
+				],
+			);
+		} else {
+			await this.sendMessage(
+				await this.getLocalizationText('understand_message'),
+			);
+		}
 	}
 }
