be29d43feb59965b99b58369e937f18e0154dd96
msinancoskun <sinancoskun2345@gmail.com>
2024-08-02 18:44:30 +0300

    IMessageEvent interface functions

--- src/interfaces/message-event.ts
+++ src/interfaces/message-event.ts
@@ -1,9 +1,9 @@
 import { TIntentPrediction } from '../types/intent-prediction';
 
 interface IMessageEvent {
-	onFileReceived?(messageObject: any): void;
-	onChatGPTMessage?(messageObject: TIntentPrediction): void;
-	onDialogflowMessage?(messageObject: TIntentPrediction): void;
+	onFileReceived(messageObject: any): void;
+	onChatGPTMessage(messageObject: TIntentPrediction): void;
+	onDialogflowMessage(messageObject: TIntentPrediction): void;
 	onDidNotUnderstand(): void;
 }
 
