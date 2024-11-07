d034e863bc6cb36ab0c84812d93c69874428c6d3
msinancoskun <sinancoskun2345@gmail.com>
2024-11-07 16:22:28 +0300

    eventHandler implementation

--- src/services/webhook.ts
+++ src/services/webhook.ts
@@ -1,9 +1,15 @@
-// Node modules.
+// ? Node modules.
 import { Request } from 'express';
 
-// Types.
+// ? Types.
 import { TCxperiumContact } from '../types/cxperium/contact';
-import { TAppLocalsServices } from '../types/base-dialog';
+import { TAppLocalsServices, TBaseDialogCtor } from '../types/base-dialog';
+import { TConversation } from '../types/conversation';
+
+// ? Services.
+import BaseConversation from './conversation';
+import { TActivity } from '../types/whatsapp/activity';
+import { EMessageEvent } from '../types/message-event';
 
 export default class {
 	req!: Request;
@@ -21,62 +27,73 @@ export default class {
 				? this.req.body.status
 				: this.req.body.event_status;
 
+			const eventStatusChange = this.req.body.event_status;
+
 			const contactId = this?.req?.body?.contactId
 				? this?.req?.body?.contactId
 				: this?.req?.body?.contact?.contactId;
-			let contact: any;
 
-			if (contactId)
-				contact =
+			if (contactId) {
+				this.contact =
 					await this.services.cxperium.contact.getContactByContactId(
 						contactId,
 					);
-			else contact = null;
+			}
 
 			switch (event) {
 				case 'NEW': {
-					this.messageCreatedEvent(event, contact);
+					this.messageCreatedEvent(event);
 					break;
 				}
 				case 'OPEN': {
-					this.messageCreatedEvent(event, contact);
+					this.messageCreatedEvent(event);
 					break;
 				}
 				case 'PENDING': {
-					this.conversationClosed(contact);
+					this.conversationClosed();
 					break;
 				}
 				case 'CLOSED': {
-					this.conversationClosed(contact);
-					break;
+					if (eventStatusChange === 'CHAT_STATUS_CHANGE') {
+						return await this.eventHandler(
+							EMessageEvent.ON_LIVE_CHAT_CLOSING,
+						);
+					}
+
+					return this.conversationClosed();
 				}
 				case 'OUTSIDE_BUSINESS_HOURS': {
-					this.outsideBusinessHours(contact);
+					this.outsideBusinessHours();
 					break;
 				}
 				case 'ASSISTANT_LOCATION_CHANGE': {
 					this.clearLanguageCache();
 					break;
 				}
+				case 'ASSISTANT_SESSION_TIMEOUT': {
+					return await this.eventHandler(
+						EMessageEvent.ON_END_OF_CHAT_SESSION,
+					);
+				}
 				case 'ASSISTANT_INTENT_CHANGE': {
 					this.clearIntentCache();
 					break;
 				}
 				case 'CHAT_NORMAL_MESSAGE': {
-					this.startMessageByLiveChatPanel(event, contact);
+					this.startMessageByLiveChatPanel(event);
 					break;
 				}
 				case 'COMPLETED':
-					this.cxperiumCloseEvent(contact);
+					this.cxperiumCloseEvent();
 					break;
 				case 'TIMEOUT':
 				case 'LEAVE':
 				case 'REPEATED': {
-					this.cxperiumCloseEvent(contact);
+					this.cxperiumCloseEvent();
 					break;
 				}
 				case 'CHAT_ASSIGNEE_CHANGE': {
-					this.chatAssignChange(contact);
+					this.chatAssignChange();
 					break;
 				}
 			}
@@ -85,59 +102,161 @@ export default class {
 		}
 	}
 
+	private getDialogRunParams(): TBaseDialogCtor {
+		const findOneDialog = this.services.dialog.getListAll.find(
+			(item: any) => {
+				return item.name == 'CXPerium.Dialogs.WhatsApp.Entry';
+			},
+		) as any;
+
+		const schemaActivity: TActivity = {
+			from: this.contact.phone,
+			message: '',
+			type: 'text',
+			userProfileName: '',
+			text: '',
+			document: {
+				id: null,
+				byteContent: Buffer.from(''),
+				mimeType: null,
+				sha256: null,
+			},
+			video: {
+				id: null,
+				mime_type: null,
+				sha256: null,
+				status: null,
+				byteContent: Buffer.from(''),
+			},
+			location: {
+				latitude: null,
+				longitude: null,
+			},
+			image: {
+				id: null,
+				byteContent: null,
+				mimeType: null,
+				sha256: null,
+			},
+			value: {
+				id: '',
+				payload: '',
+				text: '',
+			},
+			flow: {
+				isFlow: null,
+				responseJson: null,
+			},
+			isCxperiumMessage: false,
+		};
+
+		const conversation: TConversation = this.services.dialog.cache.get(
+			`CONVERSATION-${this.contact.phone}`,
+		)!;
+
+		const data: TBaseDialogCtor = {
+			contact: this.contact,
+			activity: schemaActivity,
+			conversation: new BaseConversation(
+				{
+					services: this.services,
+					contact: this.contact,
+				},
+				conversation,
+			),
+			dialogFileParams: {
+				name: findOneDialog.name,
+				path: findOneDialog.path,
+				place: `RUN MESSAGE EVENT: ${EMessageEvent.ON_END_OF_CHAT_SESSION}`,
+			},
+			services: this.services,
+		};
+
+		data.conversation.dialogFileParams = data.dialogFileParams;
+
+		return data;
+	}
+
+	private async eventHandler(event: EMessageEvent) {
+		try {
+			const mapping: Record<EMessageEvent, string> = {
+				0: 'NOT_AVAILABLE',
+				1: 'NOT_AVAILABLE',
+				2: 'NOT_AVAILABLE',
+				3: 'NOT_AVAILABLE',
+				4: 'onEndOfChatSession',
+				5: 'onClosingOfLiveChat',
+			};
+
+			const data: TBaseDialogCtor = this.getDialogRunParams();
+			const eventFunc: string = mapping[event];
+			const dialogImport = await import(data.dialogFileParams.path);
+			const dialog = new dialogImport.default(data);
+			await dialog[eventFunc]();
+		} catch (error) {
+			throw new Error(`${event} not implemented error!`);
+		}
+	}
+
 	private clearLanguageCache(): void {
 		this.services.cxperium.language.cache.del('ALL_LANGUAGES');
 	}
 
-	private chatAssignChange(contact: TCxperiumContact) {
-		this.services.cxperium.contact.updateLiveTransferStatus(contact, true);
+	private chatAssignChange() {
+		this.services.cxperium.contact.updateLiveTransferStatus(
+			this.contact,
+			true,
+		);
 	}
 
 	private clearIntentCache(): void {
 		this.services.cxperium.intent.cache.del('all-intents');
 	}
 
-	private messageCreatedEvent(event: any, contact: TCxperiumContact): void {
-		if (contact != null) {
+	private messageCreatedEvent(event: any): void {
+		if (this.contact != null) {
 			const chatId = event.chat_id?.ToString();
 			this.services.cxperium.contact.updateLiveTransferStatus(
-				contact,
+				this.contact,
 				true,
 			);
 
 			if (chatId) {
 				this.services.cxperium.contact.updateContactByCustomFields(
-					contact,
+					this.contact,
 					{ ChatId: chatId },
 				);
 			}
 		}
 	}
 
-	private conversationClosed(contact: TCxperiumContact): void {
-		this.services.cxperium.contact.updateLiveTransferStatus(contact, false);
+	private conversationClosed(): void {
+		this.services.cxperium.contact.updateLiveTransferStatus(
+			this.contact,
+			false,
+		);
 		this.services.cxperium.contact.updateSurveyTransferStatus(
-			contact,
+			this.contact,
 			false,
 		);
 
 		this.services.cxperium.session.closeActiveSessions();
 	}
 
-	private outsideBusinessHours(contact: TCxperiumContact): void {
-		this.services.cxperium.contact.updateLiveTransferStatus(contact, false);
+	private outsideBusinessHours(): void {
+		this.services.cxperium.contact.updateLiveTransferStatus(
+			this.contact,
+			false,
+		);
 	}
 
-	private startMessageByLiveChatPanel(
-		event: any,
-		contact: TCxperiumContact,
-	): void {
-		this.messageCreatedEvent(event, contact);
+	private startMessageByLiveChatPanel(event: any): void {
+		this.messageCreatedEvent(event);
 	}
 
-	private cxperiumCloseEvent(contact: TCxperiumContact) {
+	private cxperiumCloseEvent() {
 		this.services.cxperium.contact.updateSurveyTransferStatus(
-			contact,
+			this.contact,
 			false,
 		);
 	}
