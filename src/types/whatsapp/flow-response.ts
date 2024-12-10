6b735de797a9ba620d90c0d4a321d8ee73048540
eminengul <muhammedeminengul@gmail.com>
2024-12-10 10:16:21 +0300

    add type

--- src/types/whatsapp/flow-response.ts
+++ src/types/whatsapp/flow-response.ts
@@ -2,7 +2,6 @@ export type FLowResponse = {
 action: string; 
   data: Record<string, unknown>;
   amount: string; 
-  tenure: string | null; 
   flow_token: string; 
   screen: string; 
   version: string; 
