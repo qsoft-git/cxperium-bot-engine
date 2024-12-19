7519b42318270d089a32453d7d0be929e02326b0
msinancoskun <sinancoskun2345@gmail.com>
2024-12-19 20:45:19 +0300

    ExecuteFlow Interface and optional request parameter for receiveFlow, also receiveFlow typo fix

--- src/interfaces/flow.ts
+++ src/interfaces/flow.ts
@@ -2,6 +2,15 @@ import { FlowRequest } from '../types/whatsapp/flow-request';
 import { FlowResponse } from '../types/whatsapp/flow-response';
 
 export interface IFlow {
-	recieveFlow(): void;
+	receiveFlow(request?: FlowRequest): void;
 	returnResponse(request: FlowRequest): FlowResponse;
+	refreshOnBack(request: FlowRequest): FlowResponse;
+}
+
+export interface ExecuteFlow {
+	execute(
+		dialog: any,
+		request: FlowRequest | undefined,
+		intentName: string,
+	): Promise<FlowResponse>;
 }
