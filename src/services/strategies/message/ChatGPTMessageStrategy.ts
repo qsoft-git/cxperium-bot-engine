1090fa57eda9d7d784f07fedd4480eecb1238d60
msinancoskun <sinancoskun2345@gmail.com>
2024-12-23 17:01:16 +0300

    messaging strategies of chatgpt

---
+++ src/services/strategies/message/ChatGPTMessageStrategy.ts
@@ -0,0 +1,23 @@
+import { Dialog } from '../../../types/dialog';
+import { TIntentPrediction } from '../../../types/intent-prediction';
+import { EMessageEvent } from '../../../types/message-event';
+import { IMessageStrategy } from './IMessageStrategy';
+
+class ChatGptMessageStrategy implements IMessageStrategy {
+	async handle(dialog: Dialog, prediction: TIntentPrediction): Promise<void> {
+		try {
+			await dialog.services.dialog.runMessageEvent(
+				dialog,
+				prediction,
+				EMessageEvent.ON_CHATGPT_MESSAGE,
+			);
+		} catch (error: any) {
+			await dialog.services.whatsapp.message.sendRegularMessage(
+				dialog.contact.phone,
+				prediction.chatgptMessage!,
+			);
+		}
+	}
+}
+
+export { ChatGptMessageStrategy };
