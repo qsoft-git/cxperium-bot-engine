ddfe5fd947ab735f14707478a42fea2c1fd150fc
msinancoskun <sinancoskun2345@gmail.com>
2024-12-23 17:02:28 +0300

    ExecutonInterface and params mapping

--- src/types/whatsapp/flow.ts
+++ src/types/whatsapp/flow.ts
@@ -1,3 +1,6 @@
+// ? Types.
+import { EMessageEvent } from '../message-event';
+
 export type TFlowMessage = {
 	recipient_type: 'individual';
 	to: string;
@@ -31,24 +34,30 @@ export type TFlowMessage = {
 	};
 };
 
-export enum ExecutionParameters {
+export enum ExecutionInterface {
 	REFRESH_ON_BACK = 'refreshOnBack',
 	RECEIVE_FLOW = 'receiveFlow',
 	RETURN_RESPONSE = 'returnResponse',
 	RUN_DIALOG = 'runDialog',
-	ON_FILE_RECEIVED = 'onFileReceived',
-	ON_CHATGPT_MESSAGE = 'onChatGPTMessage',
-	ON_DIALOGFLOW_MESSAGE = 'onDialogflowMessage',
-	ON_DID_NOT_UNDERSTAND = 'onDidNotUnderstand',
+	ON_FILE_RECEIVED = 'ON_FILE_RECEIVED',
+	ON_CHATGPT_MESSAGE = 'ON_CHATGPT_MESSAGE',
+	ON_DIALOGFLOW_MESSAGE = 'ON_DIALOGFLOW_MESSAGE',
+	ON_DID_NOT_UNDERSTAND = 'ON_DID_NOT_UNDERSTAND',
+	ON_SESSION_TIMEOUT = 'ON_SESSION_TIMEOUT',
+	ON_CLOSING_OF_LIVE_CHAT = 'ON_CLOSING_OF_LIVE_CHAT',
+	EVENT_HANDLER = 'EVENT_HANDLER',
 }
 
-export const executionParamsMapping: Record<ExecutionParameters, string> = {
-	[ExecutionParameters.REFRESH_ON_BACK]: 'refreshOnBack',
-	[ExecutionParameters.RECEIVE_FLOW]: 'receiveFlow',
-	[ExecutionParameters.RETURN_RESPONSE]: 'returnResponse',
-	[ExecutionParameters.RUN_DIALOG]: 'runDialog',
-	[ExecutionParameters.ON_FILE_RECEIVED]: 'onFileReceived',
-	[ExecutionParameters.ON_CHATGPT_MESSAGE]: 'onChatGPTMessage',
-	[ExecutionParameters.ON_DIALOGFLOW_MESSAGE]: 'onDialogflowMessage',
-	[ExecutionParameters.ON_DID_NOT_UNDERSTAND]: 'onDidNotUnderstand',
+export const executionParamsMapping: Record<ExecutionInterface, string> = {
+	[ExecutionInterface.REFRESH_ON_BACK]: 'refreshOnBack',
+	[ExecutionInterface.RECEIVE_FLOW]: 'receiveFlow',
+	[ExecutionInterface.RETURN_RESPONSE]: 'returnResponse',
+	[ExecutionInterface.RUN_DIALOG]: 'runDialog',
+	[EMessageEvent.ON_FILE_RECEIVED]: 'onFileReceived',
+	[EMessageEvent.ON_CHATGPT_MESSAGE]: 'onChatGPTMessage',
+	[EMessageEvent.ON_DIALOGFLOW_MESSAGE]: 'onDialogflowMessage',
+	[EMessageEvent.ON_DID_NOT_UNDERSTAND]: 'onDidNotUnderstand',
+	[EMessageEvent.ON_SESSION_TIMEOUT]: 'onSessionTimeout',
+	[EMessageEvent.ON_CLOSING_OF_LIVE_CHAT]: 'onClosingOfLiveChat',
+	[ExecutionInterface.EVENT_HANDLER]: 'EVENT_HANDLER',
 };
