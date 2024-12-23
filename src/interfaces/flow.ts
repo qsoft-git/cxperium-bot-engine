3fac3843f0b62afbe6dabdf08515c5303b4f8561
msinancoskun <sinancoskun2345@gmail.com>
2024-12-23 16:51:00 +0300

    interface updated logic.

--- src/interfaces/flow.ts
+++ src/interfaces/flow.ts
@@ -1,15 +1,17 @@
+// ? Types.
+import { Dialog } from '../types/dialog';
 import { FlowRequest } from '../types/whatsapp/flow-request';
 import { FlowResponse } from '../types/whatsapp/flow-response';
 
 export interface IFlow {
-	receiveFlow(request?: FlowRequest): void;
-	returnResponse(request: FlowRequest): FlowResponse;
-	refreshOnBack(request: FlowRequest): FlowResponse;
+	receiveFlow?(request?: FlowRequest): void;
+	returnResponse?(request: FlowRequest): FlowResponse;
+	refreshOnBack?(request: FlowRequest): FlowResponse;
 }
 
 export interface ExecuteFlow {
 	execute(
-		dialog: any,
+		dialog: Dialog,
 		request: FlowRequest | undefined,
 		intentName: string,
 	): Promise<FlowResponse>;
