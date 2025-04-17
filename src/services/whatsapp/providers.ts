b7d6cb5039e0926c90ddef42b18eb89f575909cc
Fatma Atasoy <76038509+fatmaatasoy@users.noreply.github.com>
2025-04-17 15:37:14 +0300

    Bump version to 2.0.20 and improve error handling in WhatsApp provider

--- src/services/whatsapp/providers.ts
+++ src/services/whatsapp/providers.ts
@@ -36,40 +36,45 @@ export const cloudProvider = async (
 		);
 	}
 
-	const requestUrl = `${
-		process.env.PROVIDER_BASE_URL || 'https://provider.cxperium.com'
-	}/${process.env.VERSION || 'v21.0'}/${phoneNumberId}/messages`;
+	const requestUrl = `${process.env.PROVIDER_BASE_URL || 'https://provider.cxperium.com'
+		}/${process.env.VERSION || 'v21.0'}/${phoneNumberId}/messages`;
 
 	const reviveBody = { ...body, messaging_product: 'whatsapp' };
 	delete reviveBody?.recipient_type;
 	delete reviveBody?.interactive?.body?.type;
 
-	const fetchResponse = await fetch(requestUrl, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: 'Bearer ' + config.key,
-			'X-Forwarded-for': 'CXPERIUM_BOT',
-		},
-		body: JSON.stringify(reviveBody),
-	});
+	try {
+		const fetchResponse = await fetch(requestUrl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + config.key,
+				'X-Forwarded-for': 'CXPERIUM_BOT',
+			},
+			body: JSON.stringify(reviveBody),
+		});
 
-	if (!fetchResponse.ok) {
-		if (fetchResponse.status == 404) {
-			throw new Error('Provider is not reachable!');
+		if (!fetchResponse.ok) {
+			if (fetchResponse.status == 404) {
+				throw new Error('Provider is not reachable!');
+			}
+
+			throw new Error(`HTTP error! status: ${fetchResponse.status}`);
 		}
 
-		throw new Error(`HTTP error! status: ${fetchResponse.status}`);
+		const textResponse = await fetchResponse.text();
+		const response = textResponse ? JSON.parse(textResponse) : {};
+
+		console.log(
+			`Message with ${response?.botMessageId || textResponse
+			} id is sent to Meta! 🚀`,
+		);
+
+		return response?.botMessageId;
+
+	} catch (error: any) {
+		console.error('Error in provider:', error);
+		throw new Error('Error in provider: ' + error.message);
 	}
 
-	const textResponse = await fetchResponse.text();
-	const response = textResponse ? JSON.parse(textResponse) : {};
-
-	console.log(
-		`Message with ${
-			response?.botMessageId || textResponse
-		} id is sent to Meta! 🚀`,
-	);
-
-	return response?.botMessageId;
 };
